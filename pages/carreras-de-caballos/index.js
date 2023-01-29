1be6c1b43123684bafbc14fa22705fe2929c896d
Victor Sanchez <victor1305@icloud.com>
2023-01-29 20:11:31 +0100

    feat(first-steps): Add pages to project

---
+++ pages/carreras-de-caballos/index.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function HorseRaces() {
+  return (
+    <div>
+      <main className="main">
+        <h1>¿Eres nuevo en el mundo de las Carreras de Caballos?</h1>
+        <h6>En esta sección encontraras todo lo necesario para iniciarte.</h6>
+      </main>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
+import React, { useState } from 'react';
+
 export default function Home() {
   return (
     <div>
       <main className="main">
         <h1>HOLAAAAAAAAAAAA</h1>
+        <div>
+          <h4>Últimas apuestas:</h4>
+          <div>
+            
+          </div>
+        </div>
       </main>
     </div>
   );

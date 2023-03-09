6cf5421e6256341dfa8924e5907ef34b80a8c891
Victor Sanchez <victor1305@icloud.com>
2023-03-09 19:51:40 +0100

    feat(stats): fix stats problem

--- pages/stats.jsx
+++ pages/stats.jsx
@@ -176,13 +176,17 @@ export default function Stats({ start, end, statsArr }) {
                     <tr>
                       {tableHeader.map((elm, index) => {
                         if (
-                          (elm === "Aciertos" ||
-                            elm === "Fallos" ||
-                            elm === "Nulos") &&
-                          isDesktop
+                          elm === "Aciertos" ||
+                          elm === "Fallos" ||
+                          elm === "Nulos"
                         ) {
                           return (
-                            <th key={`header-${index}`}>
+                            <th
+                              key={`header-${index}`}
+                              className={
+                                styles["table-stats__bet--hide-mobile"]
+                              }
+                            >
                               <div
                                 className={classNames(
                                   styles["table-stats__bet"],
@@ -198,16 +202,25 @@ export default function Stats({ start, end, statsArr }) {
                               ></div>
                             </th>
                           );
-                        }
-                        if (
-                          ((statsType !== "Stakes" || elm !== "Stake Medio") &&
-                            isDesktop) ||
-                          elm === statsType ||
-                          elm === "Acierto" ||
-                          elm === "Yield" ||
-                          elm === "Profit"
-                        ) {
-                          return <th key={`header-${index}`}>{elm}</th>;
+                        } else {
+                          return (
+                            <th
+                              key={`header-${index}`}
+                              className={classNames({
+                                [styles["table-stats__bet--hide"]]:
+                                  statsType === "Stakes" &&
+                                  elm === "Stake Medio",
+                                [styles["table-stats__bet--hide-mobile"]]: !(
+                                  elm === statsType ||
+                                  elm === "Acierto" ||
+                                  elm === "Yield" ||
+                                  elm === "Profit"
+                                ),
+                              })}
+                            >
+                              {elm}
+                            </th>
+                          );
                         }
                       })}
                     </tr>
@@ -216,52 +229,51 @@ export default function Stats({ start, end, statsArr }) {
                     {tableBody.map((item, index) => (
                       <tr key={index}>
                         {arrKeys.map((itemKey, keyIndex) => {
-                          if (
-                            ((statsType !== "Stakes" ||
-                              itemKey !== "medium_stake") &&
-                              isDesktop) ||
-                            itemKey === "yield" ||
-                            itemKey === "profit" ||
-                            itemKey === "win_percent" ||
-                            itemKey === arrKeys[0]
-                          ) {
-                            return (
-                              <td
-                                id={`body-${index}-${keyIndex}`}
-                                key={`body-${index}-${keyIndex}`}
-                                className={classNames({
-                                  [styles["table-stats__result"]]:
-                                    itemKey === "yield" || itemKey === "profit",
-                                  [styles["table-stats__result--win"]]:
-                                    (itemKey === "yield" ||
-                                      itemKey === "profit") &&
-                                    item[itemKey] > 0,
-                                  [styles["table-stats__result--loss"]]:
-                                    (itemKey === "yield" ||
-                                      itemKey === "profit") &&
-                                    item[itemKey] < 0,
-                                  [styles["table-stats__result--void"]]:
-                                    (itemKey === "yield" ||
-                                      itemKey === "profit") &&
-                                    (item[itemKey] === "0.00" ||
-                                      item[itemKey] === "NaN"),
-                                })}
-                              >
-                                {(itemKey === "win_percent" ||
-                                  itemKey === "yield") &&
-                                item[itemKey] !== "NaN"
-                                  ? `${item[itemKey]}%`
-                                  : itemKey === "medium_stake" &&
-                                    item[itemKey] === "NaN"
-                                  ? "0.00"
-                                  : (itemKey === "win_percent" ||
-                                      itemKey === "yield") &&
-                                    item[itemKey] === "NaN"
-                                  ? "N/A"
-                                  : item[itemKey]}
-                              </td>
-                            );
-                          }
+                          return (
+                            <td
+                              id={`body-${index}-${keyIndex}`}
+                              key={`body-${index}-${keyIndex}`}
+                              className={classNames({
+                                [styles["table-stats__bet--hide"]]:
+                                  statsType === "Stakes" &&
+                                  itemKey === "medium_stake",
+                                [styles["table-stats__bet--hide-mobile"]]: !(
+                                  itemKey === "yield" ||
+                                  itemKey === "profit" ||
+                                  itemKey === "win_percent" ||
+                                  itemKey === arrKeys[0]
+                                ),
+                                [styles["table-stats__result"]]:
+                                  itemKey === "yield" || itemKey === "profit",
+                                [styles["table-stats__result--win"]]:
+                                  (itemKey === "yield" ||
+                                    itemKey === "profit") &&
+                                  item[itemKey] > 0,
+                                [styles["table-stats__result--loss"]]:
+                                  (itemKey === "yield" ||
+                                    itemKey === "profit") &&
+                                  item[itemKey] < 0,
+                                [styles["table-stats__result--void"]]:
+                                  (itemKey === "yield" ||
+                                    itemKey === "profit") &&
+                                  (item[itemKey] === "0.00" ||
+                                    item[itemKey] === "NaN"),
+                              })}
+                            >
+                              {(itemKey === "win_percent" ||
+                                itemKey === "yield") &&
+                              item[itemKey] !== "NaN"
+                                ? `${item[itemKey]}%`
+                                : itemKey === "medium_stake" &&
+                                  item[itemKey] === "NaN"
+                                ? "0.00"
+                                : (itemKey === "win_percent" ||
+                                    itemKey === "yield") &&
+                                  item[itemKey] === "NaN"
+                                ? "N/A"
+                                : item[itemKey]}
+                            </td>
+                          );
                         })}
                       </tr>
                     ))}

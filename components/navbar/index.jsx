5b7e55acdebffb023ef37ce70bae334e93f07964
Victor Sanchez <victor1305@icloud.com>
2023-03-11 00:27:09 +0100

    feat(styles): Edit bet detail styles and block mobile scroll when menu is open

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -1,34 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import RouterService from "@/lib/routerService";
 import styles from "@/components/navbar/NavBar.module.scss";
 import Link from "next/link";
 import classNames from "classnames";
 import { useRouter } from "next/router";
-import { FiMenu, FiX } from 'react-icons/fi';
+import { FiMenu, FiX } from "react-icons/fi";
 
 export default function NavBar() {
-
-  const [ open, setOpen ] = useState(false)
+  const [open, setOpen] = useState(false);
   const router = useRouter();
 
+  function preventDefault(e) {
+    e = e || window.event;
+    if (e.preventDefault) {
+      e.preventDefault();
+    }
+    e.returnValue = false;
+  }
+
   const openCloseMenu = () => {
-    const body = document.querySelector('body') 
+    const body = document.querySelector("body");
     if (!open) {
-      body.style.overflowY = 'hidden'
+      body.style.overflowY = "hidden";
+      document.addEventListener("touchmove", preventDefault, false);
     } else {
-      body.style.overflowY = 'auto'
+      body.style.overflowY = "auto";
+      document.removeEventListener("touchmove", preventDefault, false);
     }
 
-    setOpen(!open)
-  }
+    setOpen(!open);
+  };
 
   const closeMenu = () => {
-    const body = document.querySelector('body')
+    const body = document.querySelector("body");
     if (open) {
-      body.style.overflowY = 'auto'
-      setOpen(!open)
+      body.style.overflowY = "auto";
+      document.removeEventListener("touchmove", preventDefault, false);
+      setOpen(!open);
     }
-  }
+  };
 
   return (
     <div>
@@ -38,21 +48,51 @@ export default function NavBar() {
             <img src="/logo.png" alt="logo" />
           </Link>
         </div>
-        <div className={classNames([styles["container__menu"]],{
-              [styles["container__menu--open"]]: open,
-            })}>
-          <p className={classNames({
-              [styles["container__menu--active"]]: router.pathname.includes("carreras"),
-            })}><Link href={RouterService.HORSE_RACING_URL} onClick={closeMenu}>Carreras de caballos</Link></p>
-          <p className={classNames({
-              [styles["container__menu--active"]]: router.pathname.includes("stats"),
-            })}><Link href={RouterService.STATS_URL} onClick={closeMenu}>Stats</Link></p>
-          <p className={classNames({
-              [styles["container__menu--active"]]: router.pathname.includes("quienes-somos"),
-            })}><Link href={RouterService.WHO_ARE_US_URL} onClick={closeMenu}>Quienes somos</Link></p>
+        <div
+          className={classNames([styles["container__menu"]], {
+            [styles["container__menu--open"]]: open,
+          })}
+        >
+          <p
+            className={classNames({
+              [styles["container__menu--active"]]: router.pathname.includes(
+                "carreras"
+              ),
+            })}
+          >
+            <Link href={RouterService.HORSE_RACING_URL} onClick={closeMenu}>
+              Carreras de caballos
+            </Link>
+          </p>
+          <p
+            className={classNames({
+              [styles["container__menu--active"]]: router.pathname.includes(
+                "stats"
+              ),
+            })}
+          >
+            <Link href={RouterService.STATS_URL} onClick={closeMenu}>
+              Stats
+            </Link>
+          </p>
+          <p
+            className={classNames({
+              [styles["container__menu--active"]]: router.pathname.includes(
+                "quienes-somos"
+              ),
+            })}
+          >
+            <Link href={RouterService.WHO_ARE_US_URL} onClick={closeMenu}>
+              Quienes somos
+            </Link>
+          </p>
         </div>
         <div className={styles["container__burger"]}>
-          {open ? <FiX onClick={openCloseMenu} /> : <FiMenu onClick={openCloseMenu} />}
+          {open ? (
+            <FiX onClick={openCloseMenu} />
+          ) : (
+            <FiMenu onClick={openCloseMenu} />
+          )}
         </div>
       </nav>
     </div>
